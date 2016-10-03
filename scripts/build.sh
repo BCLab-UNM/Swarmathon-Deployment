276d0689ffbee45573d5605aabe7c26f2020a2bb
jjakenichol <jjaken@unm.edu>
2016-10-03 14:35:31 -0600

    Rename build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,5 @@
 source /home/swarmie/rover_workspace/devel/setup.bash
 export GAZEBO_MODEL_PATH=/home/swarmie/rover_workspace/simulation/models
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/swarmie/rover_workspace/devel/lib/
-catkin_make
+catkin clean --yes
+catkin build

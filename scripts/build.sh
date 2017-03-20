aabebe5ef9529fe0c47edc42ca7240613b9c1028
jjakenichol <jjaken@unm.edu>
2017-03-20 15:22:42 -0600

    Corrected syntax
    Lines were out of order

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+catkin clean --yes
+catkin build
 source /home/swarmie/rover_workspace/devel/setup.bash
 export GAZEBO_MODEL_PATH=/home/swarmie/rover_workspace/simulation/models
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/swarmie/rover_workspace/devel/lib/
-catkin clean --yes
-catkin build

2481daca523acff4ffd37db547807ccf264d68b4
jjakenichol <jjaken@unm.edu>
2017-03-21 16:00:12 -0600

    Ansible and shell scripts to automate team change
    buildAllTeams.sh unpacks, builds, and repacks each team's archive file in /home/swarmie/physicalTeams2017
    
    populalateArchives.yml is an ansible play for copying team archives to all swarmies, and copying and running the buildAllTeams.sh script.  This file is intended to be run after pulling and archiving all teams' code on the local machine.
    
    loadTeam.yml is a ansible play to be run during the Swarmathon competition.  This play prompts for the team's code as input, then it prepares the team's code to be run on each swarmie.  This is to be done prior to running the launch script for each robot at the beginning of each round.

--- scripts/buildAllTeams.sh
+++ scripts/buildAllTeams.sh
@@ -6,9 +6,10 @@ for filename in ./*.tar.gz; do
   tar -xvzf "$filename"
   dir=$(basename "$filename" .tar.gz)
 
-  mv $dir rover_workspace
+  mv $dir rover_workspace/
   if [ -d rover_workspace ]; then
-    cd rover_workspace
+    mv rover_workspace/ ~/rover_workspace/
+    cd ~/rover_workspace/
 
     catkin clean --yes
     catkin build
@@ -16,8 +17,10 @@ for filename in ./*.tar.gz; do
     export GAZEBO_MODEL_PATH=/home/swarmie/rover_workspace/simulation/models
     export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/swarmie/rover_workspace/devel/lib/
 
-    cd ..
-    tar -cvzf "$filename" rover_workspace
+    cd ~/
+    tar -cvzf "$filename" rover_workspace/
+    rm -rf rover_workspace/
+    mv "$filename" physicalTeams2017/"$filename"
+    cd physicalTeams2017/
   fi
-  rm -rf rover_workspace
 done
